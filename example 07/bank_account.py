09536eb598bbb93dc44630179926bf50a23970db
Ronen Shachar <ronen.shachar@gmail.com>
2025-12-30 23:05:03 +0200

    Add example 6
    Add composition

--- example 07/bank_account.py
+++ example 07/bank_account.py
@@ -1,10 +1,11 @@
-# כעת נוסיף קלאס יורש נוסף
-# שימו לב כי הוא אינו דורש שם בעל החשבון אלא מקבע אותו
+# כעת נוסיף קלאס נוסף Person
+
+from person import Person
+
 class BankAccount:
     def __init__(self, id: str, f_name: str, l_name: str):
         self.account_id: str = id
-        self.first_name: str = f_name
-        self.last_name: str = l_name
+        self.person: Person = Person(f_name, l_name)
         self.balance: float = 0.0
 
     def deposit(self, amount: float):
@@ -43,13 +44,20 @@ class BusinessBankAccount(BankAccount):
 
 bank_account1 = BankAccount('1245', 'Moshe', 'Cohen')
 business_bank_account2 = BusinessBankAccount('1000', 'Tesla')
+vip_bank_account2 = VIPBankAccount('8982', 'David', 'Lev')
 
 print("**** bank_account1")
 print( bank_account1 )
-print( bank_account1.__dict__ )
+print( bank_account1.person.get_full_name() )
 
 ## Now Business account
 print()
 print("**** business_bank_account2")
 print( business_bank_account2 )
-print( 'Pay attention to first and last name: ', business_bank_account2.__dict__ )
+print( business_bank_account2.person.get_full_name() )
+
+## Now VIP account
+print()
+print("**** vip_bank_account2")
+print( vip_bank_account2 )
+print( vip_bank_account2.person.get_full_name() )
